f89f3e9a652179216ad9b8c0f7f594af00d02aab
Camelia Brennan <cameliabrennan@gmail.com>
2020-09-23 14:09:34 -0400

    bash script

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/purchase.html templates/bottom.html > docs/purchase.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
