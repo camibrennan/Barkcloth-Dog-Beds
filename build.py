9371484543a5ad44938fa14e0420d818bc9f4ba1
Camelia Brennan <cameliabrennan@gmail.com>
2020-09-30 12:52:08 -0400

    phase 1 refactoring SSG completed (maybe)

--- build.py
+++ build.py
@@ -1,49 +1,39 @@
-
-def main():
-    top_html = open('./templates/top.html').read()
-    bottom_html = open('./templates/bottom.html').read()
-    index_html = open('./content/index.html').read()
-    combined_html = top_html + index_html + bottom_html
-    print(combined_html)
-    open('./docs/index.html', 'w+').write(combined_html)
-
-    top_html = open('./templates/top.html').read()
-    bottom_html = open('./templates/bottom.html').read()
-    blog_html = open('./content/blog.html').read()
-    combined_html = top_html + blog_html + bottom_html
-    print(combined_html)
-    open('./docs/blog.html', 'w+').write(combined_html)
-
-    top_html = open('./templates/top.html').read()
-    bottom_html = open('./templates/bottom.html').read()
-    purchase_html = open('./content/purchase.html').read()
-    combined_html = top_html + purchase_html + bottom_html
-    print(combined_html)
-    open('./docs/purchase.html', 'w+').write(combined_html)
-
-main()
-
+ 
 pages = [
  
 {
-    "filename": "content/index.html",
-    "output": "docs/index.html",
+    "filename": "./content/index.html",
+    "output": "./docs/index.html",
     "title": "Index",
 },
 {
-    "filename": "content/purchase.html",
-    "output": "docs/purchase.html",
+    "filename": "./content/purchase.html",
+    "output": "./docs/purchase.html",
     "title": "Purchase",
 },
 {
-    "filename": "content/blog.html",
-    "output": "docs/blog.html",
+    "filename": "./content/blog.html",
+    "output": "./docs/blog.html",
     "title": "Blog",
 }
 ]
 
-for page in pages:
-    print(page)
 
-page_title = page['title']
-print(page_title)
+def main():
+    for page in pages:
+        page_title = page['title']
+        page_filename = page['filename']
+        page_output = page['output']
+     
+    top_html = open('./templates/top.html').read()
+    bottom_html = open('./templates/bottom.html').read()
+    index_html = open(page_filename).read()
+    combined_html = top_html + index_html + bottom_html
+    print(combined_html)
+    open(page_output, 'w+').write(combined_html)
+
+main()
+
+
+ 
+
