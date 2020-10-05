8b40057f1adeeba07a43889ace22c8788bf085e9
Camelia Brennan <cameliabrennan@gmail.com>
2020-10-05 15:57:03 -0400

    corrected build.py to 3 functions for H3

--- build.py
+++ build.py
@@ -21,16 +21,22 @@ pages = [
 def read_template():
     template = open("templates/base.html").read()
     return template
-    
+
+def apply_template(page_filename, page_output):
+    template = read_template()
+    index_content = open(page_filename).read()
+    finished_index_page = template.replace("{{content}}", index_content)
+    open(page_output, "w+").write(finished_index_page) 
+    return finished_index_page
+
 def main():
     for page in pages:
         page_title = page['title']
         page_filename = page['filename']
         page_output = page['output']
         template = read_template()
-        index_content = open(page_filename).read()
-        finished_index_page = template.replace("{{content}}", index_content)
-        open(page_output, "w+").write(finished_index_page) 
+        finished_index_page = apply_template(page_filename, page_output)
+        
                         
 main()
 
