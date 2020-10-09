e980a61fdb1ccf794506da225126f4d7acd80ccd
Camelia Brennan <cameliabrennan@gmail.com>
2020-10-08 20:53:56 -0400

    HW4 jinja ssg phase 1

--- build.py
+++ build.py
@@ -1,44 +1,57 @@
- 
-pages = [
- 
-{
-    "filename": "content/index.html",
-    "output": "docs/index.html",
-    "title": "Index",
-},
-{
-    "filename": "content/purchase.html",
-    "output": "docs/purchase.html",
-    "title": "Purchase",
-},
-{
-    "filename": "content/blog.html",
-    "output": "docs/blog.html",
-    "title": "Blog",
-}
-]
+import glob
+import os
 
-def read_template():
-    template = open("templates/base.html").read()
-    return template
+all_html_files = glob.glob('content/*.html')
 
-def apply_template(page_filename, page_output):
-    template = read_template()
-    index_content = open(page_filename).read()
-    finished_index_page = template.replace("{{content}}", index_content)
-    open(page_output, "w+").write(finished_index_page) 
-    return finished_index_page
+def get_filename(file_path): 
+    file_name = os.path.basename(file_path)
 
-def main():
-    for page in pages:
-        page_title = page['title']
-        page_filename = page['filename']
-        page_output = page['output']
-        template = read_template()
-        finished_index_page = apply_template(page_filename, page_output)
-        
-                        
-main()
+def get_psge_title(file_path): 
+    file_name = os.path.basename(file_path)
+    name_only, extension = os.path.splitext(file_name)
+    title = name_only
+    return title
+     
+def get_output_file(file_path):  
+    file_name = os.path.basename(file_path)
+    text = file_path
+    output_file_path = text.replace('content', 'docs')
+    return output_file_path
 
+pages = []
+for file_path in all_html_files:
+    pages.append({
+    "filename": file_path,
+    "title": get_psge_title(file_path),
+    "output": get_output_file(file_path),
+    })
+    print(pages)
+
+from jinja2 import Template
+index_html = open("content/index.html").read()
+
+template_html = open("templates/base.html").read()
+template = Template(template_html)
+template.render(
+    title="title",
+    content=index_html,
+)
+
+
+
+
+# def read_template():
+#     template = open("templates/base.html").read()
+#     return template
+
+# def apply_template(page_filename, page_output, page_title):
+#     template = read_template()
+#     index_content = open(page_filename).read()
+#     finished_index_page = template.replace("{{content}}", index_content).replace("{{title}}", page_title)
+#     open(page_output, "w+").write(finished_index_page) 
+#     return finished_index_page
+
+             
+# main()
 
 
